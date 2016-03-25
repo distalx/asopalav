9331d5ceb0780d214c3a78fdebd69312532e10a4
distalx <distalx@gmail.com>
2016-03-25 23:11:53 +0530

    dummy user added to fixture

--- server/fixture.js
+++ server/fixture.js
@@ -1,5 +1,34 @@
 Meteor.startup(function () {
 
-  console.log("init");
+  if (Meteor.users.find().fetch().length === 0) {
+
+  console.log('Creating users: ');
+
+  users = [
+    {name:"User",email:"user@example.com",roles:[]},
+    {name:"moderator",email:"moderator@example.com",roles:['moderator']},
+    {name:"author",email:"author@example.com",roles:['author']},
+    {name:"Admin",email:"admin@example.com",roles:['admin']}
+  ];
+
+  _.each(users, function (userData) {
+    var id
+
+    console.log(userData);
+
+    id = Accounts.createUser({
+      email: userData.email,
+      password: "demo123",
+      profile: { name: userData.name }
+    });
+
+    // email verification
+    Meteor.users.update({_id: id},
+                        {$set:{'emails.0.verified': true}});
+
+    Roles.addUsersToRoles(id, userData.roles);
+
+  });
+
 
 });
